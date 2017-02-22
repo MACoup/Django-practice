4983f61a9f20edd595622a07a1796762c2d69948
Mark Coup <coupmark@gmail.com>
2017-02-21 17:53:25 -0700

    NEW

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
+    # added the word 'specifics'
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
